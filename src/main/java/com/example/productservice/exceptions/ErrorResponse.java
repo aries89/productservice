8afa5493b9b30998cd197d2bf30f6fe63d95b65d
Manjiri Pai <manjiri.kamat05@gmail.com>
2025-11-07 12:46:18 +1100

    Removed lombok because it is causing issues

--- src/main/java/com/example/productservice/exceptions/ErrorResponse.java
+++ src/main/java/com/example/productservice/exceptions/ErrorResponse.java
@@ -5,11 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
 public class ErrorResponse {
 	
 	private HttpStatus status;
@@ -17,15 +13,22 @@ public class ErrorResponse {
     private LocalDateTime timestamp;
     private List<String> errors;
     
+    
+    
+
+	public ErrorResponse() {
+		super();
+	}
+
 
 	public ErrorResponse(HttpStatus status, String message, LocalDateTime timestamp) {
-		super();
+		
 		this.status = status;
 		this.message = message;
 		this.timestamp = timestamp;
 	}
-    
-    
+
+
 	public ErrorResponse(HttpStatus status, String message, LocalDateTime timestamp, List<String> errors) {
 		super();
 		this.status = status;
@@ -34,6 +37,50 @@ public class ErrorResponse {
 		this.errors = errors;
 	}
 
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+
+
+	public List<String> getErrors() {
+		return errors;
+	}
+
+
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
+    
+	
+	
+    
+	
 
     
 	  
